2d1e64861268eca75d3ab43b4595ab7deda63489
martijnreus <martijn.reus@gmail.com>
2025-02-19 16:05:44 +0100

    Connect to dynamoDB

--- lambda/stores/lambda_function.py
+++ lambda/stores/lambda_function.py
@@ -1,11 +1,22 @@
 import json
+import boto3
+import os
 
 def lambda_handler(event, context):
-    response = {
-        'statusCode': 200,
-        'body': json.dumps({
-            'message': 'Hey there boys'
-        })
-    }
+    try:
+        dynamodb = boto3.resource("dynamodb", region_name=os.getenv("eu-west-1"))
+        table = dynamodb.Table("Store")
 
-    return response
+        # Scan all items from the "Stores" table
+        stores = table.scan()['Items']
+
+        return {
+            "statusCode": 200,
+            "body": json.dumps(stores)
+        }
+
+    except Exception as e:
+        return {
+            "statusCode": 500,
+            "body": json.dumps({"error": str(e)})
+        }
