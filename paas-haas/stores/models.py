3dc637cd66b797ef835d0e40db41131e2f7a3c55
Bob Reus <bobreus950@gmail.com>
2025-02-21 16:05:02 +0100

    add shared layer for python

--- paas-haas/stores/models.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel, Field, field_validator
-from bson import ObjectId
-
-class Store(BaseModel):
-    id: str = Field(default_factory=lambda: str(ObjectId()), alias="_id")
-    name: str
-    location: str
