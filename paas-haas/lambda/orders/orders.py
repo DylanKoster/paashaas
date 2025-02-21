9d41769eb8b7647704b45d767ee059bf1dbfcf1f
DylanKoster <dylankoster40@gmail.com>
2025-02-21 09:29:25 +0100

    Moved lambdas and changed code to represent API endpoint

---
+++ paas-haas/lambda/orders/orders.py
@@ -0,0 +1,54 @@
+import json
+
+# import requests
+
+
+def lambda_handler(event, context):
+    """Sample pure Lambda function
+
+    Parameters
+    ----------
+    event: dict, required
+        API Gateway Lambda Proxy Input Format
+
+        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+
+    context: object, required
+        Lambda Context runtime methods and attributes
+
+        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
+
+    Returns
+    ------
+    API Gateway Lambda Proxy Output Format: dict
+
+        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+    """
+
+    # try:
+    #     ip = requests.get("http://checkip.amazonaws.com/")
+    # except requests.RequestException as e:
+    #     # Send some context about this error to Lambda Logs
+    #     print(e)
+
+    #     raise e
+
+    if event['httpMethod'] == 'GET':
+        return {
+            'statusCode': 200,
+            'body': json.dumps('these are all the orders')
+        }
+    elif event['httpMethod'] == 'POST':
+        body = json.loads(event.get('body', '{}'))  # Parse the JSON body
+        return {
+            'statusCode': 200,
+            'body': json.dumps({
+                'message': 'Hello, World! This is a POST request to orders.',
+                'receivedData': body
+            })
+        }
+    else:
+        return {
+            'statusCode': 405,
+            'body': json.dumps('Method Not Allowed')
+        }
